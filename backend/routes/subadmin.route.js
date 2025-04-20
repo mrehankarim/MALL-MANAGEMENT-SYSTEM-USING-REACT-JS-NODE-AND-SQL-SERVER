28e361b7ae057777506e7af6768ff603fc004011
UmerMujahid <umermujahid4738@gmail.com>
2025-04-20 21:40:44 +0500

    Added revenue and expense functionality

--- backend/routes/subadmin.route.js
+++ backend/routes/subadmin.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { addCustomer, getAllShops,addShopsInBulk,allocateShopToStore, activateStore,getAllStores, insertBill, getBillsofShop,addMonthlyRentofStore,updateRent } from "../controllers/subadmin.controller.js";
+import { getExpensesOfMall,getTotalRevenueOfMall,addCustomer, getAllShops,addShopsInBulk,allocateShopToStore, activateStore,getAllStores, insertBill, getBillsofShop,addMonthlyRentofStore,updateRent } from "../controllers/subadmin.controller.js";
 import verifyJwt from "../middleware/auth.middleware.js";
 import verifySubAdmin from '../middleware/verfiySubAdmin.js'
 import { upload } from "../utils/multer.js";
@@ -16,6 +16,8 @@ router.route('/shops').get(verifyJwt,verifySubAdmin,getAllShops)
 router.route('/stores').get(verifyJwt,verifySubAdmin,getAllStores)
 router.route('/add/bill').post(verifyJwt,verifySubAdmin,insertBill)
 router.route('/bills').get(verifyJwt,verifySubAdmin,getBillsofShop)
+router.route('/revenue').get(verifyJwt,verifySubAdmin,getTotalRevenueOfMall)
+router.route('/expense').get(verifyJwt,verifySubAdmin,getExpensesOfMall)
 router.route('/add/monthlyrent').post(verifyJwt,verifySubAdmin,addMonthlyRentofStore)
 router.route('/update/rent').put(verifyJwt,verifySubAdmin,updateRent)
 export default router
